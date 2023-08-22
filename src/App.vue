37ee4c69a8b14017da8ff509e2a10e7341d1e477
CZJoanna <jyunchen199512@gmail.com>
2023-08-22 23:40:54 +0800

    feat: template example

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,112 @@
+<template>
+  <div class="p-1rem">
+    <div class="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-1rem">
+    <div 
+      v-for="roomType in roomTypes"
+      :key="roomType"
+      class="b-10 b-solid b-#f8f8f8"
+    >
+      <div class="bg-#f8f8f8">
+       <h2 class="text-center text-1rem">{{ roomType }}</h2>
+      </div>
+      <div class="p-1rem">
+        <!--平假旺 -->
+        <div class="flex flex-justify-between">
+          <div class="flex flex-col flex-items-center">
+            <span>平日</span>
+            <div>
+              <el-input-number
+                v-model="num"
+                :min="1"
+                :controls="false"
+                @change="handleChange"
+              />
+            </div>
+          </div>
+          <div class="flex flex-col flex-items-center">
+            <span>假日</span>
+            <div>
+              <el-input-number
+                v-model="num"
+                :min="1"
+                controls-position="right"
+                :controls="false"
+                @change="handleChange"
+              />
+            </div>
+          </div>
+          <div class="flex flex-col flex-items-center">
+            <span>旺日</span>
+            <div>
+              <el-input-number
+                v-model="num"
+                :min="1"
+                controls-position="right"
+                :controls="false"
+                @change="handleChange"
+              />
+            </div>
+          </div>
+        </div>
+         <hr>
+        <div class="hover:bg-#fffcea">
+          <!-- 日期 -->
+          <span class="text-gray">
+            2023/08/23 - 2023/12/31
+          </span>
+          <div>
+            <span class="bg-amber-100 p-1">一</span>
+            <span class="bg-amber-100 p-1">二</span>
+            <span class="bg-amber-100 p-1">三</span>
+          </div>
+          <!--平假旺 -->
+        <div class="flex flex-justify-between">
+          <div class="flex flex-col flex-items-center">
+            <span>平日</span>
+            <div>
+              2000
+            </div>
+          </div>
+          <div class="flex flex-col flex-items-center">
+            <span>假日</span>
+            <div>
+              3000
+            </div>
+          </div>
+          <div class="flex flex-col flex-items-center">
+            <span>旺日</span>
+            <div>
+              5000
+            </div>
+          </div>
+        </div>
+        </div>
+      
+      </div>
+    </div>
+  </div>
+  </div>
+</template>
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue'
+
+const num = ref(1)
+const handleChange = (value) => {
+  console.log(value)
+}
+
+const roomTypes = [
+  '豪華雙人房',
+  '豪華四人房',
+  '溫馨家庭房',
+  '海景雙床房'
+]
 </script>
 
-<template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-      12345
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
-</template>
-
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+::v-deep() {
+  .el-input-number {
+    width: auto;
   }
 }
 </style>
