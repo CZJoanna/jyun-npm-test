b2223522d66ec5cb5cf72bd0b7290966e7f9ae21
Jyun <tomer.rd15@gmail.com>
2023-08-23 17:17:42 +0800

    feat: template brainstorm

---
+++ src/CustomPrice.vue
@@ -0,0 +1,32 @@
+<template>
+  <!-- v-for -->
+  <div class="flex flex-items-center flex-justify-between mb-1rem">
+    <div>
+      <!-- 日期 -->
+      <span class="text-gray">
+        {{ priceInfo.date_range[0] }} ~ {{ priceInfo.date_range[1] }}
+      </span>
+      <!-- 星期 -->
+      <div>
+        <span class="bg-amber-100 p-1">日</span>
+        <span class="bg-amber-100 p-1">一</span>
+        <span class="bg-amber-100 p-1">二</span>
+      </div>
+      <!--特殊房價 -->
+      <span>特殊房價：{{ priceInfo.price }}</span>
+    </div>
+    <div class="flex flex-items-center gap-1rem">
+      <IEpEditPen class="cursor-pointer" @click="$emit('edit')"/>
+      <IEpDelete class="cursor-pointer" @click="$emit('delete')"/>
+    </div>
+  </div> 
+</template>
+
+<script setup>
+const props = defineProps({
+  priceInfo: {
+    type: Object,
+    default: () => ({})
+  }
+})
+</script>
---
+++ src/RoomCard.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="b-10 b-solid b-#f8f8f8">
+    <div class="bg-#f8f8f8">
+        <h2 class="text-center text-1rem">{{ roomType.name }}</h2>
+    </div>
+    <div class="p-1rem">
+      <!--平假旺 -->
+      <div class="flex flex-justify-between">
+        <div 
+        v-for="(dateType, i) in dateTypes" 
+        :key="dateType"
+        >
+        <div class="flex flex-col flex-items-center">
+          <span>{{ dateType }}</span>
+          <div>
+            <el-input-number
+              v-model="priceArr[i]"
+              :min="1"
+              :controls="false"
+            />
+          </div>
+        </div>
+      </div>
+      </div> 
+      <hr class="my-1rem">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  roomType: {
+    type: Object,
+    default: () => ({})
+  }
+})
+
+const { priceArr } = toRefs(props.roomType) 
+
+const dateTypes = [
+  '平日',
+  '假日',
+  '旺日'
+]
+</script>
+<style scoped>
+::v-deep() {
+  .el-input-number {
+    width: auto;
+  }
+}
+</style>
