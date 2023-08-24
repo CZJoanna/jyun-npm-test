bdbc33ae9fe65883159545b47f4f00401fbcaef1
Jyun <tomer.rd15@gmail.com>
2023-08-24 12:27:59 +0800

    chore: add eslint eslint-plugin-vue @unocss/eslint-config

---
+++ .eslintrc.cjs
@@ -0,0 +1,10 @@
+/* eslint-env node */
+module.exports = {
+  root: true,
+  "parser": "vue-eslint-parser",
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@unocss"
+  ],
+}
